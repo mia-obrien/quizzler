88336829887037269a543be9bd2e411d7c55633b
Mihri <mihribanguneydas@gmail.com>
2022-06-16 23:11:08 -0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
@@ -32,34 +31,29 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  void checkAnswer(bool userPickedAnswer) {
-    bool correctAnswer = quizBrain.getCorrectAnswer();
+  String currentQuestion = '';
+  void setQuestionText() async {
+    String temp = await quizBrain.getQuestionText();
+    setState(() {
+      currentQuestion = temp;
+    });
+  }
+
+  void checkAnswer(bool userPickedAnswer) async {
+    bool correctAnswer = await quizBrain.getCorrectAnswer();
+    bool isFinished = await quizBrain.isFinished();
 
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
-      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
-      if (quizBrain.isFinished() == true) {
-        //TODO Step 4 Part A - show an alert using rFlutter_alert,
-
-        //This is the code for the basic alert from the docs for rFlutter Alert:
-        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
-
-        //Modified for our purposes:
+      if (isFinished == true) {
         Alert(
           context: context,
           title: 'Finished!',
           desc: 'You\'ve reached the end of the quiz.',
         ).show();
 
-        //TODO Step 4 Part C - reset the questionNumber,
         quizBrain.reset();
-
-        //TODO Step 4 Part D - empty out the scoreKeeper.
         scoreKeeper = [];
-      }
-
-      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      else {
+      } else {
         if (userPickedAnswer == correctAnswer) {
           scoreKeeper.add(Icon(
             Icons.check,
@@ -88,7 +82,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.getQuestionText(),
+                currentQuestion,
+                //quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -144,9 +139,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,66 +1,61 @@
-import 'question.dart';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http_requests/http_requests.dart';
 
 class QuizBrain {
-  int _questionNumber = 0;
-
-  List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
-    Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
-    Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
-    Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
-    Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
-    Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
-  ];
-
-  void nextQuestion() {
-    if (_questionNumber < _questionBank.length - 1) {
-      _questionNumber++;
+  Future<void> nextQuestion() async {
+    Response r = await HttpRequests.get("http://localhost:8888/next");
+    print(r.success);
+    print(r.json);
+    print(r.json['next']);
+    if (r.success) {
+      return r.json['next'];
+    } else {
+      throw Exception('Failed to get next question');
     }
   }
 
-  String getQuestionText() {
-    return _questionBank[_questionNumber].questionText;
+  Future<String> getQuestionText() async {
+    Response r = await HttpRequests.get("http://localhost:8888/question");
+    print(r.success);
+    print(r.json);
+    print(r.json['question']);
+    if (r.success) {
+      print("HEEEEEEEEEEEEEEEEEEEY");
+      return r.json['question'];
+    } else {
+      throw Exception('Failed to get question');
+    }
   }
 
-  bool getCorrectAnswer() {
-    return _questionBank[_questionNumber].questionAnswer;
-  }
-
-  //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
-
-  bool isFinished() {
-    if (_questionNumber >= _questionBank.length - 1) {
-      //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
-
-      print('Now returning true');
+  Future<bool> getCorrectAnswer() async {
+    Response r = await HttpRequests.get("http://localhost:8888/answer");
+    print(r.success);
+    print(r.json);
+    print(r.json['answer']);
+    if (r.success) {
       return true;
-
     } else {
       return false;
     }
   }
 
-  //TODO: Step 4 part B - Create a reset() method here that sets the questionNumber back to 0.
-  void reset() {
-    _questionNumber = 0;
+  Future<bool> isFinished() async {
+    Response r = await HttpRequests.get("http://localhost:8888/finished");
+    print(r.success);
+    print(r.json);
+    print(r.json['finished']);
+    if (r.success) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> reset() async {
+    Response r = await HttpRequests.get("http://localhost:8888/reset");
+    print(r.success);
+    print(r.json);
+    print(r.json['reset']);
   }
 }
